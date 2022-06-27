130c3810f975f1104cb37690fd6885cbf8a3ea84
ZerOx4C <zerox4c@gmail.com>
2022-06-28 03:41:25 +0900

    event-apiがurl_verificationに応答できるようにした

--- event-api/src/main.ts
+++ event-api/src/main.ts
@@ -1,3 +1,13 @@
-function doGet(e: GoogleAppsScript.Events.DoGet): GoogleAppsScript.Content.TextOutput {
+function doPost(e: GoogleAppsScript.Events.DoPost): GoogleAppsScript.Content.TextOutput {
+	const json = JSON.parse(e.postData.contents);
+
+	if (json.token != VERIFICATION_TOKEN) {
+		throw new Error("invalid token.");
+	}
+
+	if (json.type == "url_verification") {
+		return ContentService.createTextOutput(json.challenge);
+	}
+
 	return ContentService.createTextOutput("yay!");
 }
